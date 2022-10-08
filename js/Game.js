11406c97ea28f8aec313a3e3f59fdb89d96b5543
VaatsalYadav <105976389+VaatsalYadav@users.noreply.github.com>
2022-10-08 19:52:18 +0530

    bh

--- js/Game.js
+++ js/Game.js
@@ -38,11 +38,14 @@ class Game {
     car2.scale = 0.07;
 
     cars = [car1, car2];
-
+ 
     
     fuels = new Group();
     powerCoins = new Group();
     obstacle1 = new Group(); 
+    obstacle2 = new Group()
+
+
     var obstacle1Positions = [
       { x: width / 2 - 150, y: height - 1300, image: obstacle1Image },
       { x: width / 2 + 250, y: height - 1800, image: obstacle1Image },
@@ -52,7 +55,15 @@ class Game {
       { x: width / 2, y: height - 5300, image: obstacle1Image },
     ];
 
-  
+    var obstacle2Positions = [
+      { x: width / 2 + 250, y: height - 800, image: obstacle2Image },
+      { x: width / 2 - 180, y: height - 2300, image: obstacle2Image },
+      { x: width / 2 , y: height - 2800, image: obstacle2Image },
+     
+      { x: width / 2 - 180, y: height - 4300, image: obstacle2Image },
+      { x: width / 2, y: height - 5300, image: obstacle2Image },
+    ];
+
     // Adding fuel sprite in the game
     this.addSprites(fuels, 4, fuelImage, 0.02);
 
@@ -65,7 +76,13 @@ class Game {
       0.04,
       obstacle1Positions
     );
-    
+    this.addSprites(
+      obstacle2,
+      obstacle2Positions.length,
+      obstacle2Image,
+      0.04,
+      obstacle2Positions
+    );
   }
 
   // C38 TA
